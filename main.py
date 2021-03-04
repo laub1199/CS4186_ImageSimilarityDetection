914656218dc94d7334d1c6ba65bcba3a79f65637
Sennett Lau <laub1199@gmail.com>
2021-03-05 01:18:24 +0800

    added yolo object detection algo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import time
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
@@ -55,7 +56,7 @@ def Hist(queryPath, comparePath):
         return
 
     f1 = open(queryPath + ".txt").read().split()
-    
+
     x1 = int(f1[0])
     y1 = int(f1[1])
     w1 = int(f1[2])
@@ -102,12 +103,84 @@ def Hist(queryPath, comparePath):
 
         print('Method:', compare_method, 'Perfect, Base-Half, Image-Compare :', query_query, '/', query_half, '/', query_compare)
 
+def YOLO(imagePath, confidence, threshold):
+    # confidence default 0.5, threshold default 0.3
+    LABELS = open('./yolo-coco/coco.names').read().strip().split('\n')
+
+    np.random.seed(42)
+    COLORS = np.random.randint(0, 255, size=(len(LABELS), 3), dtype="uint8")
+
+    print("[INFO] loading YOLO from disk...")
+    net = cv2.dnn.readNetFromDarknet('./yolo-coco/yolov3.cfg', './yolo-coco/yolov3.weights')
+
+    img = cv2.imread(imagePath + '.jpg')
+    (H, W) = img.shape[:2]
+
+    ln = net.getLayerNames()
+    ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+    blob = cv2.dnn.blobFromImage(img, 1 / 255.0, (416, 416), swapRB=True, crop=False)
+    net.setInput(blob)
+    start = time.time()
+    layerOutputs = net.forward(ln)
+    end = time.time()
+
+    print("[INFO] YOLO took {:.6f} seconds".format(end - start))
+
+    boxes = []
+    confidences = []
+    classIDs = []
+
+    for output in layerOutputs:
+        for detection in output:
+            scores = detection[5:]
+            classID = np.argmax(scores)
+            _confidence = scores[classID]
+            if _confidence > confidence:
+                box = detection[0:4] * np.array([W, H, W, H])
+                (centerX, centerY, width, height) = box.astype("int")
+                x = int(centerX - (width / 2))
+                y = int(centerY - (height / 2))
+                boxes.append([x, y, int(width), int(height)])
+                confidences.append(float(confidence))
+                classIDs.append(classID)
+
+    # for box in boxes:
+    #     (x, y) = (box[0], box[1])
+    #     (w, h) = (box[2], box[3])
+    #     color = [int(c) for c in COLORS[classIDs[0]]]
+    #     cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
+
+    # print(boxes)
+    # print(confidences)
+    # print(confidence)
+    # print(threshold)
+    idxs = cv2.dnn.NMSBoxes(boxes, confidences, 0.1, 0.1)
+
+    if len(idxs) > 0:
+        # loop over the indexes we are keeping
+        for i in idxs.flatten():
+            # extract the bounding box coordinates
+            (x, y) = (boxes[i][0], boxes[i][1])
+            (w, h) = (boxes[i][2], boxes[i][3])
+            # draw a bounding box rectangle and label on the image
+            color = [int(c) for c in COLORS[classIDs[i]]]
+            cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
+            text = "{}: {:.4f}".format(LABELS[classIDs[i]], confidences[i])
+            cv2.putText(img, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(0)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     queryPath = "C:/Users/laub1/Desktop/4186/Queries/"
     comparePath = "C:/Users/laub1/Desktop/4186/Images/"
-    Hist(queryPath + '01', comparePath + '0017')
+
+    YOLO(comparePath + '2595', 0.5, 0.3)
+
+    # Hist(queryPath + '01', comparePath + '0017')
+
     # for i in range(1, 2000):
     #     image = "0000" + str(i)
     #     image = image[-4:]
