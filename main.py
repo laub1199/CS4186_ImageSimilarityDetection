225d925c0ab9e9f08948af6598b0aa6869da1985
Sennett Lau <laub1199@gmail.com>
2021-03-05 13:12:41 +0800

    modified SIFT_BF function params

--- main.py
+++ main.py
@@ -6,32 +6,34 @@ import time
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-def SIFT_BF(num1, num2):
-    img1 = cv2.imread("C:/Users/laub1/Desktop/4186/Queries/" + num1 + ".jpg")
-    img2 = cv2.imread("C:/Users/laub1/Desktop/4186/Images/" + num2 + ".jpg")
+def SIFT_BF(queryPath, comparePath):
+    query = cv2.imread(queryPath + ".jpg")
+    compare = cv2.imread(comparePath + ".jpg")
 
-    if (int(num2) <= 2000):
-        f1 = open("C:/Users/laub1/Desktop/4186/Queries/" + num1 + ".txt").read().split()
-        f2 = open("C:/Users/laub1/Desktop/4186/Images/" + num2 + ".txt").read().split()
-        x1 = int(f1[0])+20
-        y1 = int(f1[1])+20
-        w1 = int(f1[2])-40
-        h1 = int(f1[3])-40
-        img1 = img1[y1:y1 + h1, x1:x1 + w1]
+    f1 = open(queryPath + ".txt").read().split()
+
+    x1 = int(f1[0])
+    y1 = int(f1[1])
+    w1 = int(f1[2])
+    h1 = int(f1[3])
+    query = query[y1:y1 + h1, x1:x1 + w1]
+
+    if int(str(comparePath).split('Images/')[1]) <= 2000:
+        f2 = open(comparePath + ".txt").read().split()
 
         x2 = int(f2[0])
         y2 = int(f2[1])
         w2 = int(f2[2])
         h2 = int(f2[3])
-        img2 = img2[y2:y2 + h2, x2:x2 + w2]
+        compare = compare[y2:y2 + h2, x2:x2 + w2]
 
-    img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2BGRA)
-    img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2BGRA)
+    query = cv2.cvtColor(query, cv2.COLOR_BGR2GRAY)
+    compare = cv2.cvtColor(compare, cv2.COLOR_BGR2GRAY)
 
     sift = cv2.xfeatures2d.SIFT_create()
 
-    keypoints_1, descriptors_1 = sift.detectAndCompute(img1, None)
-    keypoints_2, descriptors_2 = sift.detectAndCompute(img2, None)
+    keypoints_1, descriptors_1 = sift.detectAndCompute(query, None)
+    keypoints_2, descriptors_2 = sift.detectAndCompute(compare, None)
 
     bf = cv2.BFMatcher(cv2.NORM_L1, crossCheck=False)
 
@@ -42,10 +44,12 @@ def SIFT_BF(num1, num2):
         if m.distance < 0.8 * n.distance:
             good.append([m])
 
-    print(num2)
     print(len(good)/len(matches))
 
-    # img3 = cv2.drawMatchesKnn(img1, keypoints_1, img2, keypoints_2, good, img2, flags=2)
+    matchingImage = cv2.drawMatchesKnn(query, keypoints_1, compare, keypoints_2, good, compare, flags=2)
+
+    cv2.imshow("Image", matchingImage)
+    cv2.waitKey(0)
 
 def Hist(queryPath, comparePath):
     query = cv2.imread(queryPath + '.jpg')
@@ -177,9 +181,11 @@ if __name__ == '__main__':
     queryPath = "C:/Users/laub1/Desktop/4186/Queries/"
     comparePath = "C:/Users/laub1/Desktop/4186/Images/"
 
-    YOLO(comparePath + '2595', 0.5, 0.3)
+    # YOLO(comparePath + '2595', 0.5, 0.3)
+    #
+    # # Hist(queryPath + '01', comparePath + '0017')
 
-    # Hist(queryPath + '01', comparePath + '0017')
+    SIFT_BF(queryPath+"01", comparePath+"0017")
 
     # for i in range(1, 2000):
     #     image = "0000" + str(i)
